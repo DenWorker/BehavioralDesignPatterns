fe6baf3cbe881dc24864c4f593432e3de8225ef7
DenWorker <whiteskyD@yandex.ru>
2023-06-09 14:48:05 +0300

    Template pattern

---
+++ src/main/java/DenWorker/ru/template/WelcomePage.java
@@ -0,0 +1,8 @@
+package DenWorker.ru.template;
+
+public class WelcomePage extends WebsiteTemplate {
+    @Override
+    public void showPageContent() {
+        System.out.println("Welcome");
+    }
+}
